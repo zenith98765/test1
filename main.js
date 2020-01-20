a0c0ece975ce3c5e74d54e5c31dc6ef3e1e6a146
zenith <zenith98765@gmail.com>
2020-01-20 14:13:57 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+var tta = document.querySelectorAll('.tes');
+var ttb = document.getElementsByClassName('tes');
+var ttc = document.getElementsByTagName('div');
+var ttd = document.getElementById('nes2');
+var tte = document.getElementById('nes4');
+var ttf = document.getElementById('nes5');
