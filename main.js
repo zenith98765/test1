7d2cd007cb5d846cfa01fb463fe5f974b1f05ed4
zenith <zenith98765@gmail.com>
2020-01-20 14:39:27 +0530

    MoD after FeaTURe -- Added variable

--- main.js
+++ main.js
@@ -6,3 +6,4 @@ var ttc = document.getElementsByTagName('div');
 var ttd = document.getElementById('nes2');
 var tte = document.getElementById('nes4');
 var ttf = document.getElementById('nes5');
+var test1 = 32;
